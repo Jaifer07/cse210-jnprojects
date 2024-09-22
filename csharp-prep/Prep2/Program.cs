61c14c42d8d1e6edac4af6c801f78d22db8002c2
jaifer07 <nav21005@byui.edu>
2024-09-22 17:45:12 +0200

    program completed like instructions given

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage ");
+        string gradeFromUser = Console.ReadLine();
+
+        int gradePercentage = int.Parse(gradeFromUser);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter ="D";
+        }
+        else 
+        {
+            letter = "F";
+        }   
+        
+        Console.WriteLine($"Your grade is {letter}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! you pass the class");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you did'nt pass the class.We encourage you to improve.");
+        }
     }
 }
