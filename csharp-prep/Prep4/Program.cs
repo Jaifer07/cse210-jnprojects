a697926b125ad6f4abb636f12b448f9ddc09c1be
jaifer07 <nav21005@byui.edu>
2024-09-25 00:21:22 +0200

    I need help with this error

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+   static void Main(string[] args)
+  {   
+    Console.WriteLine("Enter a list of numbers, type 0 when finished"); 
+
+     List<int> numbers = new List<int>();
+     int response = -1;
+     do
+     {
+         Console.Write("Enter a number ");
+         response = int.Parse(Console.ReadLine());
+     } while (response != 0);     
+     
+
+      for (int i = 0; i < numbers.Count; i++)
+      {
+      Console.WriteLine(numbers[i]);
+      }
+      
+      // Computing the sum 
+
+      int sum = 0;
+      foreach (int number in numbers)
+      {
+        sum += number;        
+      }
+
+     Console.WriteLine($"The sum is {sum}"); 
+
+     //Calculating the average
+
+     float average = ((float)sum) / numbers.Count;
+     Console.Writeline($"The average is: {average}");
+
+     int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max number is: {max}");
+  }
 }
