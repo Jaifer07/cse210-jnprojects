4913604f69945b10a05f21214933d6a909ff3f57
jaifer07 <nav21005@byui.edu>
2024-10-18 02:03:34 +0200

    program completed as instructions given

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+       Fraction fration1 = new Fraction();
+       Console.WriteLine(fration1.GetFractionString());
+       Console.WriteLine(fration1.GetdecimalValue());   
+
+       Fraction fraction2 = new Fraction(5);       
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetdecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetdecimalValue());
+       
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetdecimalValue());
+
+
     }
 }
